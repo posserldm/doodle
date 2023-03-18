be543cd4eb91199fe8f224242e9ed15bde527f89
posser <1782342391@qq.com>
2023-03-18 21:51:25 +0800

    第一版:只能画东西，界面还丑

--- app/src/main/java/posserldm/doodle/panit/MainPaintSurfaceView.kt
+++ app/src/main/java/posserldm/doodle/panit/MainPaintSurfaceView.kt
@@ -1,76 +1,57 @@
 package posserldm.doodle.panit
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
-import android.view.SurfaceHolder
-import android.view.SurfaceView
 import android.view.View
 
-class MainPaintSurfaceView(context: Context, attr: AttributeSet): SurfaceView(context, attr),SurfaceHolder.Callback, View.OnTouchListener {
+class MainPaintView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
 
-    private val surfaceHolder: SurfaceHolder = holder
-    private val paint: Paint = Paint()
-    private var canvas: Canvas? = null
-    private var startX = 0.0F
-    private var startY = 0.0F
-    private var endX = 0.0F
-    private var endY = 0.0F
-
-    init {
-        surfaceHolder.addCallback(this)
-        setOnTouchListener(this)
-        paint.color = Color.RED
-        paint.strokeWidth = 5f
-
+    private var path: Path = Path()
+    private var paint: Paint = Paint().apply {
+        color = Color.BLACK
+        isAntiAlias = true
+        strokeWidth = 10f
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
     }
 
-    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
-        when (event?.action) {
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                startX = event.x
-                startY = event.y
+                path.moveTo(event.x, event.y)
             }
             MotionEvent.ACTION_MOVE -> {
-                endX = event.x
-                endY = event.y
-                drawLine(startX, startY, endX, endY)
-                startX = endX
-                startY = endY
+                path.lineTo(event.x, event.y)
             }
             MotionEvent.ACTION_UP -> {
-                endX = event.x
-                endY = event.y
-                drawLine(startX, startY, endX, endY)
+                path.lineTo(event.x, event.y)
             }
         }
+        invalidate()
         return true
     }
 
-    private fun drawLine(startX: Float, startY: Float, endX: Float, endY: Float) {
-        Log.i("main", "draw line method be called!")
-        canvas = surfaceHolder.lockCanvas()
-        try {
-            canvas?.drawLine(startX, startY, endX, endY, paint)
-        } finally {
-            if (canvas != null) {
-                surfaceHolder.unlockCanvasAndPost(canvas)
-            }
-        }
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawPath(path, paint)
     }
 
-    override fun surfaceCreated(holder: SurfaceHolder) {
-
+    fun clear() {
+        path.reset()
+        invalidate()
     }
 
-    override fun surfaceChanged(holder: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-    }
-
-    override fun surfaceDestroyed(p0: SurfaceHolder) {
-
+    fun getBitmap(): Bitmap {
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        draw(canvas)
+        return bitmap
     }
 }
